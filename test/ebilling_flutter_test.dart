535215566ee3902f31047d952a745df9a7e3200a
AGBONON Kankoe Yaovi <Gabi@MacBook-pro-AGBONON.local>
2021-07-21 13:46:00 +0100

    Implementation methode initPay

--- test/ebilling_flutter_test.dart
+++ test/ebilling_flutter_test.dart
@@ -3,7 +3,7 @@ import 'package:ebilling_flutter/ebilling_flutter.dart';
 
 void main() {
   test('adds one to input values', () async{
-    final ebillingPaymentService = EbPaymentService(sharedKey: '0513f7b4-3d2f-4688-b52c-04cfb45ab373', userName: 'yoboresto', env: EbPaymentService.LAB);
+    final ebillingPaymentService = EbPaymentService(sharedKey: '', userName: '', env: EbPaymentService.LAB);
 
     var payment = new Payment(500, 'Test',  'a345789os12', 'kankoe@gmail.com', '+24162649525', '', 'Kankoe');
 
